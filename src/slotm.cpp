2a1efec383611375067ffde920990a5969bdcf3c
Ace_Radom <sichenradomlyu@gmail.com>
2022-11-08 20:03:10 +0100

    push slotm lib (still in progress)

--- src/slotm.cpp
+++ src/slotm.cpp
@@ -1,1 +1,28 @@
 #include"slotm.h"
+
+SOLTMACHINE::SOLTMACHINE( PIN _Motion_Sensor , PIN _Start_Handle ,
+                          uint16_t _MAX_CLT = MAX_CONTINUOUS_LOST_TIME ){
+    Motion_Sensor = _Motion_Sensor;
+    Start_Handle  = _Start_Handle;
+
+    MAX_CLT = _MAX_CLT;
+
+    pinMode( Start_Handle , INPUT_PULLUP );
+
+    randomNUM = 0;
+    continuous_lost_time = 0;
+
+    return;
+}
+
+/**
+ * \brief judge if start handle is pulled down (by judging the button on the top's status)
+ * \return start handle (or the button) status, pulled (or pressed) as true
+ */
+bool SOLTMACHINE::is_Start_Handle_pulled(){
+    if ( digitalRead( Start_Handle ) == LOW )
+    {
+        return true;
+    }
+    return false;
+}
--- src/slotm.h
+++ src/slotm.h
@@ -1,4 +1,28 @@
 #ifndef _SLOTM_H_
 #define _SLOTM_H_
 
+#include<Arduino.h>
+
+#define MAX_CONTINUOUS_LOST_TIME 20U
+
+typedef uint8_t PIN;
+
+class SOLTMACHINE{
+    public:
+        SOLTMACHINE( PIN , PIN , uint16_t );
+
+        bool is_Start_Handle_pulled();
+    
+    private:
+        uint16_t randomNUM;
+        uint16_t continuous_lost_time;
+
+        uint16_t MAX_CLT; // MAX_Continuous_Lost_Time setting (in class)
+
+        PIN Motion_Sensor;
+        PIN Start_Handle;
+
+
+};
+
 #endif
